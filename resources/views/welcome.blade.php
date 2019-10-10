9983921760cb73608b60c67e453d5bfdbf0c9a4a
BorjaRomeroFernandez <1415.romero.borja@gmail.com>
2019-10-10 15:32:19 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,25 +9,82 @@
   <link rel="stylesheet" href="css/bootstrap.min.css" />
 
   <style>
-    .jumbotron {
-      /*background-image: url("img/port.jpg");*/
-      background-size: 100%;
+
+    nav.navbar {
+      background-color: whitesmoke;
+    }
+
+    input.text {
+      background-color: whitesmoke;
     }
 
     .icon {
-      background-image: url(ico/carro32.ico);
-      background-size: 120%;
+      height: 32px;
+      width: 32px;
+      margin-top: -10px;
+      background-image: url(ico/carro32.png);
+      background-repeat: no-repeat;
     }
 
     .icon:hover {
-      background-image: url(ico/carrol32.ico);
+      background-image: url(ico/carrol32.png);
     }
 
 
     img.camiseta {
-      height: 40%;
+      height: auto;
       width: auto;
       box-shadow: 4px 4px 5px grey;
+    }
+
+    img.camven {
+      height: auto;
+      width: 80%;
+      box-shadow: 4px 4px 5px grey;
+    }
+
+    .btn-primary {
+      background-color: darkgreen;
+      border-color: darkgreen;
+    }
+
+    .btn-primary:hover {
+      background-color: limegreen;
+      border-color: limegreen;
+    }
+
+    a {
+      color: darkgreen;
+    }
+
+    a:hover {
+      color: limegreen;
+    }
+
+    li.nav-item {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+
+    input.form-inline {
+      margin-right: 5px;
+    }
+
+    footer {
+      background-color: whitesmoke;
+      bottom: 0px;
+      padding-left: 0px;
+      padding-right: 0px;
+      width:100%;
+      height: auto;
+    }
+
+    .ventas {
+      box-shadow: 4px 4px 5px grey;
+      height: 100%;
+      background-color: whitesmoke;
+      border-color: black;
+      
     }
   </style>
 
@@ -39,26 +96,21 @@
   <!-- LAYOUT: HEADER -->
 
   <!-- SECTION: Title -->
-  <div class="jumbotron text-center">
-    <h1>UCAmisetas</h1>
-  </div>
 
   <!-- SECTION: Main menu -->
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <nav class="navbar navbar-expand-sm">
     <!-- Removing navbar-expand-sm shows this as a collapsed menu -->
-    <a class="navbar-brand" href="#">Barra de navegación</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <a class="navbar-brand" href="#"><strong>UCAmisetas</strong></a>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link" href="#">Camisetas</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Ofertas</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Categorías</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Lo último</a></li>
+        <input type="text" class="form-inline" placeholder="Buscar">
+        <button type="submit" class="btn btn-primary">Buscar</button>
       </ul>
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item"><a class="nav-link" href="#">Autentificación</a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><div class="icon responsive"></a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Autenticación</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><div class="icon"></a></li>
       </ul>
     </div>
   </nav>
@@ -67,125 +119,188 @@
   <div class="container-fluid" style="margin-top:30px">
     <div class="row">
 
-      <!-- BLOCK: LEFT -->
-      <div class="col-sm-3">
-        <!-- col-sm-3 means three out of twelve columns -->
-        <!-- SECTION: Some links -->
-        <h3>Categorías</h3>
-        <ul class="nav nav-pills flex-column">
-          <li class="nav-item"><a class="nav-link active" href="#">Todas</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Hombre</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Mujer</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Joven</a></li>
-        </ul>
-        <hr class="d-sm-none" /> <!-- d-sm-none means that the element will show only when the screen is small -->
-      </div>
-
       <!-- BLOCK: CENTER -->
-      <div class="col-sm-7">
+      <div class="col-sm-10">
         <!-- col-sm-7 means seven out of twelve columns -->
         <!-- SECTION: Entries -->
         <div class="card-body text-center">
-          <h2>Últimas oportunidades</h2>
+          <h2>Ofertas del día</h2>
           <h5>¡Corre, que vuelan!</h5>
         </div>
 
+        <!-- OFERTAS -->
+
         <div class="row">
-          <div class="card-body col-sm-4 text-center">
-            <img class="camiseta" src="img/cam1.jpg">
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cam1.jpg"></a>
             <br>
-            <p>Camiseta Neo Tokyo</p>
-            <strong> 19'00 € </strong>
+            <p><strong>Camiseta Neo Tokyo</strong></p>
+            <div style="text-decoration: line-through;">19'00 €</div>
+            <strong>13'99 €  - 26% Dcto.!</strong>
           </div>
-          <div class="card-body col-sm-4 text-center">
-            <img class="camiseta" src="img/cam2.jpg">
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cam2.jpg"></a>
             <br>
-            <p>Camiseta Roll Iniciative</p>
-            <strong> 15'99 € </strong>
+            <p><strong>Camiseta Roll Iniciative</strong></p>
+            <div style="text-decoration: line-through;">15'99 €</div>
+            <strong>10'00 € - 37% Dcto.!</strong>
           </div>
-          <div class="card-body col-sm-4 text-center">
-            <img class="camiseta" src="img/cam3.jpg">
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cam3.jpg"></a>
             <br>
-            <p>Camiseta Bounty Hunter</p>
-            <strong> 17'99 € </strong>
+            <p><strong>Camiseta Bounty Hunter</strong></p>
+            <div style="text-decoration: line-through;">17'99 €</div>
+            <strong>15'00 € - 17% Dcto.!</strong>
+          </div>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cam4.jpg"></a>
+            <br>
+            <p><strong>Camiseta Rawr</strong></p>
+            <div style="text-decoration: line-through;">13'99 €</div>
+            <strong>9'99 € - 29% Dcto.!</strong>
           </div>
         </div>
+      
+        <!-- CATÁLOGO -->
         <br>
-        <br>
+
         <div class="card-body text-center">
           <h2>Catálogo</h2>
           <h5>Nuestras camisetas</h5>
         </div>
+
         <div class="row">
-          <div class="card-body col-sm-4 text-center">
-            <img class="camiseta" src="img/cat1.jpg">
-            <p>Camiseta Schwifty</p>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat1.jpg"></a>
+            <p><strong>Camiseta Schwifty</strong></p>
+            <strong>19'99 €</strong>
+          </div>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat2.jpg"></a>
+            <p><strong>Camiseta Burn the City</strong></p>
+            <strong>24'99 €</strong>
+          </div>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat3.jpg"></a>
+            <p><strong>Camiseta Lok'tar Ogar</strong></p>
+            <strong>17'99 €</strong>
+          </div>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat4.jpg"></a>
+            <p><strong>Camiseta Cthulhu</strong></p>
             <strong> 19'00 € </strong>
           </div>
-          <div class="col-sm-4 text-center">
-            <img class="camiseta" src="img/cat2.jpg">
-            <p>Camiseta Burn the City</p>
+        </div>
+
+        <div class="row">
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat5.jpg"></a>
+            <p><strong>Camiseta Great Ramen</strong></p>
             <strong> 15'99 € </strong>
           </div>
-          <div class="col-sm-4 text-center">
-            <img class="camiseta" src="img/cat3.jpg">
-            <p>Camiseta Lok'tar Ogar</p>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat6.jpg"></a>
+            <p><strong>Camiseta No Internet</strong></p>
             <strong> 17'99 € </strong>
           </div>
-          <div class="row">
-            <div class="col-sm-4 text-center">
-              <img class="camiseta" src="img/cat4.jpg">
-              <p>Camiseta Cthulhu</p>
-              <strong> 19'00 € </strong>
-            </div>
-            <div class="col-sm-4 text-center">
-              <img class="camiseta" src="img/cat5.jpg">
-              <p>Camiseta Great Ramen</p>
-              <strong> 15'99 € </strong>
-            </div>
-            <div class="col-sm-4 text-center">
-              <img class="camiseta" src="img/cat6.jpg">
-              <p>Camiseta No Internet</p>
-              <strong> 17'99 € </strong>
-            </div>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat7.jpg"></a>
+            <p><strong>Camiseta Skynet</strong></p>
+            <strong> 22'00 € </strong>
+          </div>
+          <div class="card-body col-sm-3 text-center">
+            <a href="#"><img class="camiseta" src="img/cat8.jpg"></a>
+            <p><strong>Camiseta Link In Park</strong></p>
+            <strong> 20'00 € </strong>
           </div>
         </div>
+        <div class="text-center" style="margin-bottom: 10px">
+        <button type="submit" class="btn btn-primary">Ver más camisetas</button>
+        </div>
+      </div>
 
-        <!-- BLOCK: RIGHT -->
-        <div class="col-sm-2 sidenav">
-          <!-- col-sm-2 means two out of twelve columns -->
-          <!-- SECTION: Cards -->
-          <h2>Cards</h2>
-          <!-- Style are directly included in the style attribute just for illustrative reasons. -->
-          <!-- However, including them in <style> or in a CSS file is a better practice. -->
-          <div class="card card-body bg-faded" style="background-color: #0ff; margin-bottom:15px;">
-            <h4 class="card-title">Card 1</h4>
-            <p class="card-text">Text of card 1.</p>
-            <a href="#" class="btn btn-primary">This is a Button</a>
-          </div>
-          <div class="card card-body bg-light" style="margin-bottom:15px;">
-            <h4 class="card-title">Card 2</h4>
-            <div class="fakeimg">Fake card 2 image</div>
-            <p class="card-text">Text of card 2.</p>
-          </div>
+      <!-- BLOCK: RIGHT -->
+
+      <div class="card-body col-sm-2 text-center ventas">
+        <h2>Lo más vendido</h2>
+        <div class="card-body text-center">
+          <a href="#"><img class="camven" src="img/cat9.jpg"></a>
+          <strong>Camiseta Stranger Things</strong>
+          <br>
+          <strong>17'00 €</strong>
+        </div>
+        <div class="card-body text-center">
+          <a href="#"><img class="camven" src="img/cat5.jpg"></a>
+          <strong>Camiseta Great Ramen</strong>
+          <br>
+          <strong> 15'99 € </strong>
+        </div>
+        <div class="card-body text-center">
+          <a href="#"><img class="camven" src="img/cat8.jpg"></a>
+          <strong>Camiseta Link In Park</strong>
+          <br>
+          <strong> 20'00 € </strong>
+        </div>
+        <div class="card-body text-center">
+          <a href="#"><img class="camven" src="img/cat10.jpg"></a>
+          <strong>Camiseta Slytherin</strong>
+          <br>
+          <strong> 20'00 € </strong>
+        </div>
+        <button type="submit" class="btn btn-primary">Ver más camisetas</button>
+      </div>
+    </div>
+  </div>
+
+  <footer class="page-footer font-small blue pt-4">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+        <div class="col-md-3 mb-md-0 mb-3">
+          <h5 class="text-uppercase">Acerca de</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Quiénes somos</a></li>
+            <li><a href="#!">Trabaja con nosotros</a></li>
+            <li><a href="#!">Aviso legal</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 mb-md-0 mb-3">
+          <h5 class="text-uppercase">Navegación</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Idiomas / Languages</a></li>
+            <li><a href="#!">Mapa del sitio</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 mb-md-0 mb-3">
+          <h5 class="text-uppercase">Ayuda</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Gestionar compras</a></li>
+            <li><a href="#!">Tarifas y políticas de envío</a></li>
+            <li><a href="#!">Devolver o reemplazar productos</a></li>
+            <li><a href="#!">Contacto para ayuda</a></li>
+            <li><a href="#!">IVA sobre los bienes</a></li>
+            <li><a href="#!">Métodos de pago</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 mb-md-0 mb-3">
+          <h5 class="text-uppercase">Contacto</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Atención al cliente</a></li>
+            <li><a href="#!">Twitter</a></li>
+            <li><a href="#!">Facebook</a></li>
+            <li><a href="#!">YouTube</a></li>
+            <li><a href="#!">Instagram</a></li>
+          </ul>
         </div>
       </div>
     </div>
+  </footer>
 
-    <!-- LAYOUT: FOOTER -->
-    <div class="jumbotron text-center" style="margin-bottom:0;padding:20px">
-      <div class="col-sm-4">
-
-      </div>
-    </div>
-
-    <!-- Loading Javascripts -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>
-      window.jQuery || document.write('https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>')</script>
-    <!-- <script src="js/popper.min.js"></script> -->
-    <script src="js/bootstrap.min.js"></script>
+  <!-- Loading Javascripts -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script>
+    window.jQuery || document.write('https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"')>
+  </script>
+  <!-- <script src="js/popper.min.js"></script> -->
+  <script src="js/bootstrap.min.js"></script>
 </body>
-
 </html>
